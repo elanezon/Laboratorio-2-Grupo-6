35f5232f26fe5774dc0705bb65dbc1f31afa2867
svega106 <svega106@gmail.com>
2023-03-23 16:33:27 -0600

    Adding C files

---
+++ permutation.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void) {
+    for (j = 0; j < SIZE; j++)
+        for (i = 0; i < SIZE; i++)
+            A[i][j] = i + j;
+
+    return 0;
+}
---
+++ unrolling.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void) {
+    for (i = 0; i < SIZE; i+=2)
+        for (j = 0; j < SIZE; j+=2){
+            A[i][j] = i + j;
+            A[i+1][j+1] = i + j;
+        }
+
+    return 0;
+}
