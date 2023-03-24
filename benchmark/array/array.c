7ea8962eb52c0fbc97b591fa30da3e5b9e2920bb
svega106 <svega106@gmail.com>
2023-03-23 21:29:09 -0600

    Changing array.c

--- benchmark/array/array.c
+++ benchmark/array/array.c
@@ -13,8 +13,48 @@ void baseline(void)
             A[i][j] = i + j;
 }
 
+int permutation(void) {
+
+    int i, j;
+
+    for (j = 0; j < SIZE; j++)
+        for (i = 0; i < SIZE; i++)
+            A[i][j] = i + j;
+
+    return 0;
+}
+
+int unrolling(void) {
+
+    int i, j;
+    
+    for (i = 0; i <= SIZE; i+=2){
+        for (j = 0; j <= SIZE; j+=2){
+            A[i][j] = i + j;
+            A[i+1][j+1] = i + j;
+        }
+    }
+    return 0;
+}
+
+int tiling(void) {
+
+    int i, j;
+
+    for (i = 0; i <= SIZE; i+=2){
+        for (j = 0; j <= SIZE; j+=2){
+            A[i][j] = i + j;
+            A[i+1][j+1] = i + j;
+        }
+    }
+    return 0;
+}
+
 int main(void)
 {
     baseline();
+    permutation();
+    unrolling();
+    tiling();
     return 0;
 }
